a7225315099e7464af02acc0ab5d58395a03cea0
Rachelle DeMan <97637778+demanr@users.noreply.github.com>
2022-08-22 12:06:04 -0400

    Removed ability to cause several spike death animations

--- scripts/Spikes.gd
+++ scripts/Spikes.gd
@@ -1,11 +1,22 @@
-extends StaticBody2D
+extends Area2D
 
+var KILLEDPLAYER = preload("res://scenes/SpikeDeath.tscn")
 
 func _ready():
 	pass
 
+# Spike detects player
+func _on_Spike1_body_entered(body):
+	if PlayerVars.health > 0:
+		#send to spawn
+		PlayerVars.health = -1
+		PlayerVars.respawn = true
+		killedPlayer()
 
-func _on_Area2D_body_entered(body):
-	#reset player pos to door pos
-	get_tree().change_scene("res://scenes/Spawn.tscn")
+func killedPlayer():
+	#Instance death animation
+	var death = KILLEDPLAYER.instance()
+	get_parent().add_child(death)
+	#moves death instance to current position
+	death.global_transform = global_transform
 	queue_free()
